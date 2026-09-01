26bd5fd58a93138166dc311deee396dab033695f
Hamad tarawa <tourt2031@gmail.com>
2026-09-01 11:23:56 +0300

    feat: update application properties and dependencies for OpenAI integration and enhance hotel inquiry handling

--- src/main/java/com/example/hotalproject/AI/SpringAiHotelService.java
+++ src/main/java/com/example/hotalproject/AI/SpringAiHotelService.java
@@ -2,15 +2,15 @@ package com.example.hotalproject.AI;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+
 import org.springframework.ai.chat.metadata.Usage;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.document.DocumentReader;
-import org.springframework.ai.reader.TextReader;
-import org.springframework.ai.transformer.splitter.TextSplitter;
-import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
+
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -41,59 +41,7 @@ public class SpringAiHotelService implements HotelAIService {
         this.vectorStore = vectorStore;
     }
 
-    @Value("file://${HOME}/documents/my-document.txt")
-    private Resource documentResource;
 
-    public String getRulesFor(String hotelName, String question) {
-
-        var searchRequest = SearchRequest
-                .builder()
-                .query(question)
-                .topK(6)
-                .similarityThreshold(0.0)
-                .filterExpression(
-                        new FilterExpressionBuilder()
-                                .eq(
-                                        "hotelName",
-                                        normalizeHotelName(hotelName)
-                                )
-                                .build()
-                )
-                .build();
-
-        System.err.println(
-                "Search request: " + searchRequest
-        );
-
-        List<Document> similarDocs =
-                vectorStore.similaritySearch(searchRequest);
-
-        log.info("Retrieved documents: {}", similarDocs.size());
-
-        for (Document document : similarDocs) {
-            log.info("Retrieved document text: {}", document.getText());
-            log.info("Retrieved metadata: {}", document.getMetadata());
-        }
-
-        if (similarDocs.isEmpty()) {
-            return "NO_HOTEL_DOCUMENTS_FOUND";
-        }
-
-        return similarDocs.stream()
-                .map(Document::getText)
-                .collect(
-                        Collectors.joining(
-                                System.lineSeparator()
-                        )
-                );
-    }
-
-    private String normalizeHotelName(String hotelName) {
-
-        return hotelName
-                .toLowerCase()
-                .replace(" ", "_");
-    }
     @Override
     public Answer askQuestion(Question question) {
 
@@ -102,11 +50,38 @@ public class SpringAiHotelService implements HotelAIService {
                         question.hotelName()
                 );
 
-        var relevantDocuments =
-                getRulesFor(question.hotelName(), question.question());
+
         log.info("Hotel Information:\n{}", hotelInformation);
-        log.info("Relevant Documents:\n{}", relevantDocuments);
+
         log.info("Question:\n{}", question.question());
+
+        var hotelMatch = String.format(
+                "hotelName == '%s'",
+                question.hotelName());
+
+        var advisor = RetrievalAugmentationAdvisor.builder()
+
+                .queryTransformers(
+                        TranslationQueryTransformer.builder()
+                                .chatClientBuilder(chatClient.mutate())
+                                .targetLanguage("English")
+                                .build(),
+
+                        RewriteQueryTransformer.builder()
+                                .chatClientBuilder(chatClient.mutate())
+                                .build()
+
+                )
+                .documentRetriever(
+                        VectorStoreDocumentRetriever.builder()
+                                .vectorStore(vectorStore)
+                                .similarityThreshold(0.0)
+                                .topK(6)
+                                .build()
+                )
+
+
+                .build();
 
         var responseEntity = chatClient.prompt()
 
@@ -114,12 +89,12 @@ public class SpringAiHotelService implements HotelAIService {
                         .text(systemPromptTemplate)
                         .param("hotelName", question.hotelName())
                         .param("hotelInformation", hotelInformation)
-                        .param("relevantDocuments", relevantDocuments))
+                )
 
                 .user(userSpec -> userSpec
                         .text(questionPromptTemplate)
                         .param("question", question.question()))
-
+                .advisors(advisor)
 
                 .call()
 
